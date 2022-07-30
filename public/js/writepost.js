68141571356948c3afc6204f1ee15477ae0ba00f
huntersteffner <huntersteffner@gmail.com>
2022-07-30 12:27:19 -0400

    Fixed async posting bug. Login/logout works better

--- public/js/writepost.js
+++ public/js/writepost.js
@@ -1,25 +1,30 @@
 const postForm = document.getElementById('post-form')
 
-postForm.addEventListener('submit', async (e) => {
+const makePost = async (e) => {
     e.preventDefault()
     
     const post_name = document.getElementById('post_name').value
     const post_description = document.getElementById('post_description').value
 
     if(post_name && post_description) {
-        try {
-            fetch('./api/posts/', {
-                method: 'POST',
-                body: JSON.stringify({
-                    post_name, post_description,
+        const response = await fetch('./api/posts/', {
+            method: 'POST',
+            body: JSON.stringify({post_name,post_description}),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
 
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                }
-            }).then(document.location.replace('./'))
-        } catch (err) {
-            console.log(err)
+            
+        })
+
+        if(response.ok) {
+            document.location.replace('/')
+        } else {
+            alert(response.statusText)
         }
+
     }
-})
+}
+
+postForm.addEventListener('submit', makePost)
+
